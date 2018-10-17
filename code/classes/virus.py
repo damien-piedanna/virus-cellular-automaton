05a020a7631ed5f180b17d1c8461712930fbf046
Nicolas Meyniel <37573253+nicolas-meyniel@users.noreply.github.com>
2018-10-17 10:17:19 +0000

    ajout des differents virus

--- code/classes/virus.py
+++ code/classes/virus.py
@@ -4,7 +4,31 @@
 class Virus:
     def __init__(self, label):
         self.label = label
-        if (self.label == "Peste noire"):
-            self.tauxReproduction = 12.5
+        if (self.label == "Bougnoulie"):
+            self.tauxReproduction = 666
+
+        elif (self.label == "Peste noire"):
+            self.tauxReproduction = 15
+
+        elif (self.label == "Rougeole"):
+            self.tauxReproduction = 12
+
+        elif (self.label == "Coqueluche"):
+            self.tauxReproduction = 10
+
+        elif (self.label == "Rougeole"):
+            self.tauxReproduction = 8
+
+        elif (self.label == "Variole"):
+            self.tauxReproduction = 6
+
+        elif (self.label == "VIH"):
+            self.tauxReproduction = 4
+
+        elif (self.label == "Grippe"):
+            self.tauxReproduction = 2  
+
         else: # Virus inconnu
             self.tauxReproduction = 5
+
+#END Virus
